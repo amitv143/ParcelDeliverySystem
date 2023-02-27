c65bdaaa64b7a6c0d5c31f68dae3d2e6243bf4e8
INDIA\2115296 <verma.amit5@tcs.com>
2023-02-27 21:31:52 +0530

    Added unit test for the solution.

--- ParcelDelivery.Service.Impl.UnitTest/DeliveryTest/ParcelDeliveryServiceTest.cs
+++ ParcelDelivery.Service.Impl.UnitTest/DeliveryTest/ParcelDeliveryServiceTest.cs
@@ -3,6 +3,7 @@ using Moq;
 using ParcelDelivery.Model.Payload.Request;
 using ParcelDelivery.Service.Impl.Contract;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ParcelDelivery.Service.Impl.UnitTest.DeliveryTest
 {
@@ -10,71 +11,105 @@ namespace ParcelDelivery.Service.Impl.UnitTest.DeliveryTest
     public class ParcelDeliveryServiceTest
     {
         private ParcelDeliveryService parcelDeliveryService;
-        private readonly Mock<IOrganization> _mockMrganization =new Mock<IOrganization>() ;
-
-
-
-        [TestInitialize]
-        public void TestInitialize()
-        {
-            parcelDeliveryService = new ParcelDeliveryService(_mockMrganization.Object);
-
-            _mockMrganization.Setup(X => X.Departments).Returns(new List<Department>
-                {
+        private readonly Mock<IOrganization> _mockMrganization = new Mock<IOrganization>();
+        private readonly List<Department> departmentList =
+           new List<Department>
+               {
                     new InsuranceDepartment(),
                     new MailDepartment(),
                     new RegularDepartment(),
                     new HeavyDepartment(),
                     new AddDepartment()
-                });
+               };
+
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+
+            parcelDeliveryService = new ParcelDeliveryService(_mockMrganization.Object);
+
+            _mockMrganization.Setup(X => X.Departments).Returns(departmentList);
         }
 
         [TestMethod]
-        public void WhenInvokedWithValidRequest_ShouldReturnSuccessResponse()
+        public void WhenInvokedWithWeightLessThan1_ShouldReturnMailDept()
         {
             //Arrange
-          
+
             Parcel parcel = new Parcel();
             parcel.Weight = 0.2;
             parcel.Value = 100;
+            _mockMrganization.Setup(x => x.GetSignerDepartments()).Returns(new List<Department>());
+            _mockMrganization.Setup(x => x.GetProcessorDepartments()).Returns(departmentList);
             //Act
-            var result = parcelDeliveryService.Send( parcel);
-
+            var result = parcelDeliveryService.Send(parcel);
+            var dept = result.ParcelDepartmentsFlow.FirstOrDefault().ToString();
             //Assert
+            Assert.IsTrue(dept.Split('.').Contains("MailDepartment"));
             Assert.IsNotNull(result);
             Assert.IsTrue(result.IsSent);
         }
 
         [TestMethod]
-        public void WhenInvokedWithWeight_ShouldReturnSuccessResponse()
+        public void WhenInvokedWithWeightUpTo10_ShouldReturRegularDept()
         {
             //Arrange
             Parcel parcel = new Parcel();
             parcel.Weight = 2;
             parcel.Value = 100;
+
+            _mockMrganization.Setup(x => x.GetSignerDepartments()).Returns(new List<Department>());
+            _mockMrganization.Setup(x => x.GetProcessorDepartments()).Returns(departmentList);
 
             //Act
             var result = parcelDeliveryService.Send(parcel);
 
+            var dept = result.ParcelDepartmentsFlow.FirstOrDefault().ToString();
             //Assert
+            Assert.IsTrue(dept.Split('.').Contains("RegularDepartment"));
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.IsSent);
+        }
+
+        [TestMethod]
+        public void WhenInvokedWithWeightGreaterThan10_ShouldReturHeavyDept()
+        {
+            //Arrange
+            Parcel parcel = new Parcel();
+            parcel.Weight = 200;
+            parcel.Value = 100;
+
+            _mockMrganization.Setup(x => x.GetSignerDepartments()).Returns(new List<Department>());
+            _mockMrganization.Setup(x => x.GetProcessorDepartments()).Returns(departmentList);
+
+            //Act
+            var result = parcelDeliveryService.Send(parcel);
+
+            var dept = result.ParcelDepartmentsFlow.FirstOrDefault().ToString();
+            //Assert
+            Assert.IsTrue(dept.Split('.').Contains("HeavyDepartment"));
             Assert.IsNotNull(result);
             Assert.IsTrue(result.IsSent);
         }
 
 
         [TestMethod]
-        public void WhenInvokedWithValueGreaterThan100_ShouldReturnSuccessResponse()
+        public void WhenInvokedWithValueGreaterThan100_ShouldReturnInsuranceDept()
         {
             //Arrange
-    
+
             Parcel parcel = new Parcel();
             parcel.Weight = 2;
             parcel.Value = 10001;
 
+            _mockMrganization.Setup(x => x.GetSignerDepartments()).Returns(departmentList);
+            _mockMrganization.Setup(x => x.GetProcessorDepartments()).Returns(new List<Department>());
             //Act
-          var result = parcelDeliveryService.Send(parcel);
-
+            var result = parcelDeliveryService.Send(parcel);
+            var dept = result.ParcelDepartmentsFlow.FirstOrDefault().ToString();
             //Assert
+            Assert.IsTrue(dept.Split('.').Contains("InsuranceDepartment"));
             Assert.IsNotNull(result);
             Assert.IsTrue(result.IsSent);
         }
